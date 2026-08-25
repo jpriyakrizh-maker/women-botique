5901fdc1b7d07e3b6a81a5c3396c2f26b60c01a8
Jeyapriya <jpriyakrizh@gmail.com>
2026-08-25 10:26:34 +0530

    Fix GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-//https://github.com/jpriyakrizh-maker/women-botique1.git
 export default defineConfig({
   plugins: [react()],
-  base:"/women/",
+  base: "/women-botique/",
 })
