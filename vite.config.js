15fd695ef51d4aed225741a246bedd00ef579980
Jeyapriya <jpriyakrizh@gmail.com>
2026-08-25 10:22:00 +0530

    Update Lavender Boutique website

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+//https://github.com/jpriyakrizh-maker/women-botique1.git
 export default defineConfig({
   plugins: [react()],
   base:"/women/",
